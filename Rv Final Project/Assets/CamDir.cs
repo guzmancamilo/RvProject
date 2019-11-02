b3089e6771c6a05a74a245d6fa5042f14b81829a
guzmancamilo <jcamiloguz1@gmail.com>
2019-11-01 22:44:56 -0500

    Paredes y camara

---
+++ Rv Final Project/Assets/CamDir.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamDir : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public float speed =20f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir= new Vector3(0f,0f,0f);
+        dir.y+=speed*Input.GetAxis("Mouse X");
+        dir.x-=speed*Input.GetAxis("Mouse Y");
+        transform.rotation=dir;
+    }
+}
