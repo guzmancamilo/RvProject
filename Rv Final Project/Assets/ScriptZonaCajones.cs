92ace9e92acccf63152f3f4290c46a76ecf8a5fb
guzmancamilo <jcamiloguz1@gmail.com>
2019-11-09 12:16:27 -0500

    Prueba

---
+++ Rv Final Project/Assets/ScriptZonaCajones.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptZonaCajones : MonoBehaviour
+{
+    public GameObject referenciaMensaje;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "PlayerController") {
+            Debug.Log("Entro a la zona de cajones!");
+            referenciaMensaje.active = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.name == "PlayerController") {
+            Debug.Log("salio a la zona de cajones!");
+            referenciaMensaje.active = false;
+        }
+    }
+}
