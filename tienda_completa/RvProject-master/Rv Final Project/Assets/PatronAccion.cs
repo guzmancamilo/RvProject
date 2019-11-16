737542ec77bdf675a3aa6cb3baf8462584ff508e
dilan987 <dilan200869@gmail.com>
2019-11-16 13:06:16 -0500

    Tienda
    local terminado con scripts

---
+++ tienda_completa/RvProject-master/Rv Final Project/Assets/PatronAccion.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatronAccion : MonoBehaviour
+{
+    // Public variables
+    public GameObject ObjetoActivador;
+   
+    public float TiempoDespuesDeAccion;
+    // Private variables
+    
+    private float timeToEnd;
+    private bool workDone;
+    
+
+    void Start()
+    {
+        // Justo al inicio deshabilitamos el script (será activado por el script 
+        // 'ComportamientoPatron.cs' cuando el objeto activador llegue al hito)
+        this.enabled = false;
+        
+        timeToEnd = 0;
+        workDone = false;
+
+    }
+
+
+    void Update()
+    {
+        if (!workDone)
+        {
+           
+
+
+            // No realizamos la acción hasta que no pase el tiempo 'TiempoAntesDeAccion'
+            //timeToStart >= TiempoAntesDeAccion
+            if (Input.GetKey(KeyCode.W))
+            {
+                // IMPLEMENTACIÓN DE LA ACCIÓN...
+                // ...
+                   
+
+                workDone = true;
+            }
+        }
+        else
+        {
+            timeToEnd += Time.deltaTime;
+
+            //No avanzamos al siguiente hito hasta que no pase el tiempo 'TiempoDespuesDeAccion'
+            if (timeToEnd >= TiempoDespuesDeAccion)
+            {
+                // Se inicializa el script
+                Start();
+
+                // Indicamos que se puede pasar el siguiente hito
+                ObjetoActivador.GetComponentInParent<ComportamientoPatron>().CanGoToNextMilestone = true;
+            }
+        }
+    }
+
+
+
+}
