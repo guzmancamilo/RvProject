788eb312bec3e0d2dd9830247008fab0b21a6085
dilan987 <dilan200869@gmail.com>
2019-11-16 12:17:13 -0500

    tienda

---
+++ Rv Final Project/Assets/scripts/PatronAccion.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatronAccion : MonoBehaviour
+{
+    // Public variables
+    public GameObject ObjetoActivador;
+    public float TiempoAntesDeAccion;
+    public float TiempoDespuesDeAccion;
+    public string text = "";
+    // Private variables
+    private float timeToStart;
+    private float timeToEnd;
+    private bool workDone;
+
+
+    void Start()
+    {
+        // Justo al inicio deshabilitamos el script (será activado por el script 
+        // 'ComportamientoPatron.cs' cuando el objeto activador llegue al hito)
+        this.enabled = false;
+
+        timeToStart = 0;
+        timeToEnd = 0;
+        workDone = false;
+    }
+
+
+    void Update()
+    {
+        if (!workDone)
+        {
+            timeToStart += Time.deltaTime;
+
+            // No realizamos la acción hasta que no pase el tiempo 'TiempoAntesDeAccion'
+            if (timeToStart >= TiempoAntesDeAccion)
+            {
+                // IMPLEMENTACIÓN DE LA ACCIÓN...
+                // ...
+                text = "patriarcado";
+
+                workDone = true;
+            }
+        }
+        else
+        {
+            timeToEnd += Time.deltaTime;
+
+            //No avanzamos al siguiente hito hasta que no pase el tiempo 'TiempoDespuesDeAccion'
+            if (timeToEnd >= TiempoDespuesDeAccion)
+            {
+                // Se inicializa el script
+                Start();
+
+                // Indicamos que se puede pasar el siguiente hito
+                ObjetoActivador.GetComponentInParent<ComportamientoPatron>().CanGoToNextMilestone = true;
+            }
+        }
+    }
+
+
+
+}
---
+++ Rv Final Project/Assets/scripts/screens.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screens : MonoBehaviour
+{
+    public string text;
+    public TextMesh pantalla;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
